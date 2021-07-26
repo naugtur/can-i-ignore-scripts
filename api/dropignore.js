5271a5e54c44c5687b6d15caecf2ed1bc68b0ae9
naugtur <naugtur@gmail.com>
2021-07-27 00:45:12 +0200

    change format to make room for explanations

--- api/dropignore.js
+++ api/dropignore.js
@@ -1,4 +1,4 @@
-const okToIgnore = require('../okToIgnore.json');
+const okToIgnore = Object.keys(require('../okToIgnore.json'));
 module.exports = (req, res) => {
     const packages = req.query.packages.split(',');
     res.setHeader('Cache-Control', 'max-age=0, s-maxage=86400');
