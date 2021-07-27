dad5d8304b909e4e17748f60b399687fe5bc0f8b
naugtur <naugtur@gmail.com>
2021-07-27 22:54:11 +0200

    try waiting for the asynchronous action actually

--- api/oktoignore.js
+++ api/oktoignore.js
@@ -3,9 +3,9 @@ const Redis = require("ioredis");
 const redis = new Redis(redisUrl);
 
 const okToIgnore = Object.keys(require('../okToIgnore.json'));
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
     const packages = req.query.packages.split(',');
-    redis.sadd('packages', packages);
+    await redis.sadd('packages', packages);
     res.setHeader('Cache-Control', 'max-age=0, s-maxage=86400');
     res.status(200).json(packages.filter(pkg => okToIgnore.includes(pkg)));
 };
