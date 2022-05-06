d36d23371b01f22914dfd5682c29bec6b183e69a
Thomas Broyer <t.broyer@ltgt.net>
2022-05-07 00:18:29 +0200

    Allow retrieving the whole data.json from the server
    When no packages are passed in, return the whole data
    without filtering.

--- api/check.js
+++ api/check.js
@@ -4,12 +4,18 @@ const redis = new Redis(redisUrl);
 const data = require('../data.json');
 
 module.exports = async (req, res) => {
-    const packages = req.query.packages.split(',');
-    await redis.sadd('packages', packages);
+    const packages = req.query.packages?.split(',') ?? [];
+    let result;
+    if (packages.length === 0) {
+        result = data
+    } else {
+        await redis.sadd('packages', packages);
+        result = packages.reduce((result, pkg) => {
+            result.keep[pkg] = data.keep[pkg]
+            result.ignore[pkg] = data.ignore[pkg]
+            return result;
+        }, { keep: {}, ignore: {} })
+    }
     res.setHeader('Cache-Control', 'max-age=0, s-maxage=86400');
-    res.status(200).json(packages.reduce((result, pkg) => {
-        result.keep[pkg] = data.keep[pkg]
-        result.ignore[pkg] = data.ignore[pkg]
-        return result;
-    }, { keep: {}, ignore: {} }));
+    res.status(200).json(result);
 };
